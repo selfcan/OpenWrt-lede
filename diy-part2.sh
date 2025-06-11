66da70c6f86139c005ffa471c4578325721be090
selfcan <99958756+selfcan@users.noreply.github.com>
2025-06-11 13:36:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,6 +7,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
-#find ./ | grep Makefile | grep mosdns | xargs rm -f
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
+git clone https://github.com/selfcan/openwrt-packages.git package/openwrt-packages
